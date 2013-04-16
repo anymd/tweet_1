522635d33ecc2d753e336bbaadd7b8478270bc2d
Apprentice <apprentice@devbootcamp.com>
2013-04-15 17:17:17 -0700

    Added loading gif and number of followers information

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,9 +4,11 @@ get '/' do
 end
 
 get '/:username' do
+  erb :index
+end
 
+get '/:username/tweets' do
   @user = TwitterUser.find_or_create_by_username(params[:username])
-  
   if @user.tweets.empty?
     fetch_tweets!
   else
@@ -14,34 +16,34 @@ get '/:username' do
       delete_tweets!
       fetch_tweets!
     end
-  end  
-
+  end 
   @tweets = @user.tweets.limit(10)  
+  erb :_tweets, layout: false
+end
 
-  erb :index
+get '/:username/follower_count' do
+  @user = Twitter.user(params[:username])
+  erb :_follower_count, layout: false
 end
 
 helpers do
 
   def fetch_tweets!
+    sleep 5
     Twitter.user_timeline("#{@user.username}", :count => 10).each do |status|
       Tweet.create(:text => status.text,
                    :twitter_user_id => @user.id,
                    :tweet_time => status.created_at)
     end
-     
   end
 
   def tweets_stale?
-    # Time.now - @user.tweets.last.created_at >= 20
-    
     @later_time_difference = Time.now - @user.tweets[0].tweet_time
     @earlier_time_difference = @user.tweets[0].tweet_time - @user.tweets[1].tweet_time
     @later_time_difference > @earlier_time_difference
   end
 
   def delete_tweets!
-    #@user.tweets.delete_all 
     @user.tweets.destroy_all 
   end
 
